30528dfac8fc4605692c467bf58d6918c6375b80
Sahil Patel <sahilshahpatel@gmail.com>
2020-12-19 21:22:40 -0600

    Set up basic GUI for play/pause and timeline

--- godot/Scenes/Game/game.gd
+++ godot/Scenes/Game/game.gd
@@ -1,8 +1,33 @@
 extends Node2D
 
+var is_paused: bool = false
 
 func _ready():
 	fill_tilemaps()
+	init_ui()
+	$Timer.start()
+
+
+func update_state():
+	print("Updating to state: ", $HUD/Timeline.value)
+	
+	if $HUD/Timeline.value >= len(Global.gamelog["states"]):
+		# Should never reach here if init_ui set max_value properly
+		return
+	
+	var state = Global.gamelog["states"][$HUD/Timeline.value]
+	
+	# TODO: update state
+
+
+func end_of_game():
+	# TODO: display some end-of-game thing
+	print("GAME OVER")
+	get_tree().quit()
+
+
+func init_ui():
+	$HUD/Timeline.max_value = 5#len(Global.gamelog["states"])
 
 
 func fill_tilemaps():
@@ -23,3 +48,21 @@ func fill_tilemaps():
 	var p2 = tileset.find_tile_by_name("Player 2")
 	$Map/Characters.set_cell(init_pos[0]["x"], init_pos[0]["y"], p1)
 	$Map/Characters.set_cell(init_pos[1]["x"], init_pos[1]["y"], p2)
+
+
+func _on_Timer_timeout():
+	if is_paused:
+		return
+	elif $HUD/Timeline.value == $HUD/Timeline.max_value:
+		end_of_game()
+	else:
+		update_state()
+		$HUD/Timeline.value += 1
+
+
+func _on_PlayButton_pressed():
+	is_paused = not is_paused
+	if is_paused:
+		$HUD/PlayButton.text = "Play"
+	else:
+		$HUD/PlayButton.text = "Pause"
